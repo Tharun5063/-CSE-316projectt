dd8a347552c6be351a5ba7cc0d507594e05bb03b
Tharun5063 <37810128+Tharun5063@users.noreply.github.com>
2018-04-10 21:08:46 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 #define MAX 1000
 #define TIME_QUANTUM 2 
-int flag[MAX],arrival_time[MAX],burst_time[MAX],priority[MAX],Response_Time[MAX],ft[MAX],fe[MAX],fe_flag[MAX],process_id[MAX],tms,qt[MAX];
+int temp[MAX],arr_t[MAX],bur_t[MAX],prior[MAX],Resp_t[MAX],ft[MAX],fe[MAX],fe_flag[MAX],p_id[MAX],tms,qt[MAX];
 queue<int> q;  
-void RoundRobin()
+void RR()
 {
      if(!q.empty())
       {
-        if(Response_Time[q.front()]>0 && qt[q.front()]<4)
+        if(Resp_t[q.front()]>0 && qt[q.front()]<4)
         {
                 qt[q.front()]++;
-                Response_Time[q.front()]--;
-                if(Response_Time[q.front()]==0)
+                Resp_t[q.front()]--;
+                if(Resp_t[q.front()]==0)
                 {
                 ft[q.front()]=tms+1;
                 q.pop();
                 }
-                if(Response_Time[q.front()]!=0 && qt[q.front()]==4)
+                if(Resp_t[q.front()]!=0 && qt[q.front()]==4)
                 {
                 qt[q.front()]=0;
                 q.push(q.front());
@@ -28,29 +28,26 @@ void RoundRobin()
             }
       }
 }
- 
 int main()
 {
     int i=0,n=0,smallest=0,last_smallest=-1,min,sum=0,large=0;
     printf("Enter the Number of Processes:\n");
     scanf("%d",&n);
+    int avg;
+     int wait=0;
     for(i=0;i<n;i++)
     {       printf("Enter the process id here:\n");
-            scanf("%d",&process_id[i]);
-            printf("Enter the arrival time of process_id %d :\n",process_id[i]);
-            scanf("%d",&arrival_time[i]);
-            printf("Enter the burst time of process_id %d :\n",process_id[i]);
-            scanf("%d",&burst_time[i]);
-            printf("Enter the priority time of process_id %d :\n",process_id[i]);
-            scanf("%d",&priority[i]);
- 
- 
-           
-            if(arrival_time[i]>large)
-                large=arrival_time[i];
-              sum+=burst_time[i];
-              Response_Time[i]=burst_time[i];
-             
+            scanf("%d",&p_id[i]);
+            printf("Enter the arrival time of process id %d :\n",p_id[i]);
+            scanf("%d",&arr_t[i]);
+            printf("Enter the burst time of process id %d :\n",p_id[i]);
+            scanf("%d",&bur_t[i]);
+            printf("Enter the priority time of process id %d :\n",p_id[i]);
+            scanf("%d",&prior[i]);          
+            if(arr_t[i]>large)
+                large=arr_t[i];
+              sum+=bur_t[i];
+              Resp_t[i]=bur_t[i];             
         printf("\n-----------------------------------------------------\n");
     }
     min=MAX;
@@ -60,21 +57,21 @@ int main()
       smallest=-1;
       for(i=0;i<n;i++)
       {
-        if(arrival_time[i]<=tms && priority[i]<min && Response_Time[i]>0 && !flag[i])
+        if(arr_t[i]<=tms && prior[i]<min && Resp_t[i]>0 && !temp[i])
         {
-            min=priority[i];
+            min=prior[i];
                 smallest=i;
             }
       }
       if(smallest==-1 && !q.empty())
       {
-        if(last_smallest !=-1 && Response_Time[last_smallest]==0)
+        if(last_smallest !=-1 && Resp_t[last_smallest]==0)
         {
             ft[last_smallest]=tms;
-                flag[last_smallest]=1;
+                temp[last_smallest]=1;
             }
             last_smallest=-1;
-            RoundRobin();
+            RR();
             continue;
       }
       else if(smallest!=-1 && !q.empty() && last_smallest==-1)
@@ -87,35 +84,33 @@ int main()
       }
       if(smallest!=-1 && !fe_flag[smallest])
       {
-        fe[smallest]=tms-arrival_time[smallest];
+        fe[smallest]=tms-arr_t[smallest];
         fe_flag[smallest]=1;
       }
-      if( smallest!=last_smallest && last_smallest!=-1 && !flag[last_smallest])
+      if( smallest!=last_smallest && last_smallest!=-1 && !temp[last_smallest])
       {
         q.push(last_smallest);
-        flag[last_smallest]=1;
+        temp[last_smallest]=1;
       }
       if(smallest !=-1)
-        Response_Time[smallest]--;
+        Resp_t[smallest]--;
      
-      if((smallest !=-1) && ((Response_Time[smallest]==0) ||(burst_time[smallest]-Response_Time[smallest])==TIME_QUANTAM))
+      if((smallest !=-1) && ((Resp_t[smallest]==0) ||(bur_t[smallest]-Resp_t[smallest])==TIME_QUANTUM))
       {
-        if((burst_time[smallest]-Response_Time[smallest])==TIME_QUANTAM && Response_Time[smallest]!=0)
+        if((bur_t[smallest]-Resp_t[smallest])==TIME_QUANTUM && Resp_t[smallest]!=0)
         {
-            flag[smallest]=1;
+            temp[smallest]=1;
             q.push(smallest);
             }
         else if(smallest!=-1)
         {
                 ft[smallest]=tms+1;
-                flag[smallest]=1;
+                temp[smallest]=1;
             }
       }
       last_smallest=smallest;
     }
    for(int i=0;i<n;i++)
-   printf("\n The Process Id: %d | Waiting Time : %d | Finish Time : %d | TurnAround Time : %d\n",process_id[i],fe[i],ft[i],ft[i]-burst_time[i]-arrival_time[i]);
-     
-   
-}
- 
+    
+   printf("\n  Process Id: %d | Waiting Time : %d | Finish Time : %d | TurnAround Time : %d\n",p_id[i],fe[i],ft[i],ft[i]-bur_t[i]-arr_t[i]);
+   }
