7c5efe4f8eb0c5fa4bc26340d8f01aeeacacfcd7
Tharun5063 <37810128+Tharun5063@users.noreply.github.com>
2018-04-10 20:03:54 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -119,25 +119,3 @@ int main()
    
 }
  
-int main()
-{
-	int n;
-	printf("Enter the number of processes-");
-	scanf("%d",&n);
-	struct process l[n];
-    printf("\n PROCESS\tPRIORITY\tBURST-TIME\tARRIVALTIME\n");
-	for(int i=0;i<n;i++)
-	{printf("Enter the process id");
-	  scanf("%d",&l[i].pid);
-	  printf("Enter the priority ");
-	  scanf("%d",&l[i].priority);
-	  printf("Enter the burst time");
-	  scanf("%d",&l[i].burst);
-	  printf("Enter the arrival time");
-	  scanf("%d",&l[i].arrival);
-	  printf("\n");
-		
-	}
-	display(n,l);
-	
-}
